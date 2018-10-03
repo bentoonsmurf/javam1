10a340f840d4f0cef73987d32fd427040a234c1e
Utilisateur <user@debian.arcahe.ovh>
2018-10-03 14:46:02 +0200

    duxiemme comit

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,18 +1,21 @@
 public class Fraction {
  
-	private float a, b;
+	private int a, b;
  
+	private static Fraction  ZERO =new Fraction(0,1);
+	private static Fraction  UN =new Fraction(1,1);	
+	
 	public Fraction() {
 		a = 0;
 		b = 1;
 	}
  
-	public Fraction(float a) {
+	public Fraction(int a) {
 		this.a = a;
 		this.b = 1;
 	}
  
-	public Fraction(float a, float b) {
+	public Fraction(int a, int b) {
 		this.a = a;
 		if (b != 0)
 			this.b = b;
@@ -20,19 +23,19 @@ public class Fraction {
 			this.b = 1;
 	}
  
-	public void setA(float a) {
+	public void setA(int a) {
 		this.a = a;
 	}
  
-	public void setB(float b) {
+	public void setB(int b) {
 		this.b = b;
 	}
  
-	public float getA() {
+	public int getA() {
 		return this.a;
 	}
  
-	public float getB() {
+	public int getB() {
 		return this.b;
 	}
  
@@ -46,6 +49,12 @@ public class Fraction {
 		this.b = this.a * f.getB();
 	}
  
+	public double getint() {
+		return  1.0*(this.a/this.b);
+		
+		
+	}
+	
 	public static void main(String[] args){
 		Fraction fraction = new Fraction(5,6);
 		fraction.Mult(new Fraction(3, 2));
