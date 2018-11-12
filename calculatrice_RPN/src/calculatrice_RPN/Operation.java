7595e0cdf37b0e58c20cb678d73286b73c4c6899
zbabaedjou <babaedjouziadath@gmail.com>
2018-11-12 18:12:04 +0100

    suppression des ckasses element et chiffre_relle

--- calculatrice_RPN/src/calculatrice_RPN/Operation.java
+++ calculatrice_RPN/src/calculatrice_RPN/Operation.java
@@ -2,7 +2,7 @@ package calculatrice_RPN;
 
 
 // il manque extends element 
-public abstract class Operation extends element {// enum =Class avec un nombre d'instances limité
+public abstract class Operation {// enum =Class avec un nombre d'instances limité
 	// déclaration des constantes
 	final char PLUS='+';
 	final char MOINS='-';
