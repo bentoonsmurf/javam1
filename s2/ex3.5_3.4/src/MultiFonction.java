46afc777aa39d1083027b7916ac6ffc6b0f6aaec
Utilisateur <user@debian.arcahe.ovh>
2019-02-08 16:42:32 +0100

    sdfghjkl

---
+++ s2/ex3.5_3.4/src/MultiFonction.java
@@ -0,0 +1,6 @@
+
+public interface MultiFonction {
+	void scan();
+	void copy();
+	void fax();
+}
---
+++ s2/ex3.5_3.4/src/SimplePrinter.java
@@ -0,0 +1,18 @@
+
+public class SimplePrinter implements Printer{
+
+	public void print() {
+		
+	}
+	public void scan() {
+		
+	}
+	public void copy() {
+		
+	}
+	public void fax() {
+		
+	}
+	
+	
+}
